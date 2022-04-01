a0914b8bbd6b447fae701f6b0ca30efc9688f233
Balaji Chandupatla <balajichandupatla1@gmail.com>
2022-04-01 17:11:06 -0400

    initial commit

---
+++ XFUploadFile.Server/Controllers/UploadFileController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using Microsoft.Extensions.Logging;
+
+namespace XFUploadFile.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UploadFileController : ControllerBase
+    {
+        CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=balajijobmanager;AccountKey=PEye5mFF0HFxGFzFZXWJ+qKJL6TnO8UVrDxpP0laLQEFMm3EcLt1+APxa+9S9edfdQYlPj2fSa5lQ03V3pt3+w==;EndpointSuffix=core.windows.net");
+
+        private readonly ILogger<UploadFileController> _logger;
+        private readonly IWebHostEnvironment _environment;
+
+        public UploadFileController(ILogger<UploadFileController> logger,
+            IWebHostEnvironment environment)
+        {
+            _logger = logger;
+            _environment = environment ?? throw new ArgumentNullException(nameof(environment));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            try
+            {
+                var httpRequest = HttpContext.Request;
+
+                if (httpRequest.Form.Files.Count > 0)
+                {
+                    foreach (var file in httpRequest.Form.Files)
+                    {
+                        var filePath = Path.Combine(_environment.ContentRootPath, "uploads");
+
+                        if (!Directory.Exists(filePath))
+                            Directory.CreateDirectory(filePath);
+
+                        using (var memoryStream = new MemoryStream())
+                        {
+                            await file.CopyToAsync(memoryStream);   
+                            System.IO.File.WriteAllBytes(Path.Combine(filePath, file.FileName), memoryStream.ToArray());
+
+                            await UploadToAzureAsync(file);
+                        }
+
+                        return Ok();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error");
+                return new StatusCodeResult(500);
+            }
+
+            return BadRequest();
+        }
+
+        private async Task UploadToAzureAsync(IFormFile file)
+        {
+            var cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+
+            var cloudBlobContainer = cloudBlobClient.GetContainerReference("uploads");
+
+            if (await cloudBlobContainer.CreateIfNotExistsAsync())
+            {
+                await cloudBlobContainer.SetPermissionsAsync(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Off });
+            }
+
+            var cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(file.FileName);
+            cloudBlockBlob.Properties.ContentType = file.ContentType;
+
+            await cloudBlockBlob.UploadFromStreamAsync(file.OpenReadStream());
+        }
+    }
+}
---
+++ XFUploadFile/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace XFUploadFile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var file = await MediaPicker.PickPhotoAsync();
+
+            if (file == null)
+                return;
+
+            var content = new MultipartFormDataContent();
+            content.Add(new StreamContent(await file.OpenReadAsync()), "file", file.FileName);
+
+            var httpClient = new HttpClient();
+            var response = await httpClient.PostAsync("http://192.168.2.36:3276/UploadFile", content);
+            
+
+            StatusLabel.Text = response.StatusCode.ToString();
+        }
+    }
+}
